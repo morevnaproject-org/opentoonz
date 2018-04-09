1314bfebae8f419c0dec92170d040684f6448477
Ivan Mahonin <bh@icystar.com>
2018-04-09 16:37:06 +0700

    #assistants: update FullColorBrushTool

--- toonz/sources/toonz/sceneviewerevents.cpp
+++ toonz/sources/toonz/sceneviewerevents.cpp
@@ -540,6 +540,7 @@ void SceneViewer::onMove(const TMouseEvent &event) {
       getInputManager()->trackEvent(
         0, 0, pos, &event.m_pressure, NULL,
         false, TToolTimer::ticks() );
+      getInputManager()->processTracks();
       m_tabletState = OnStroke;
     } else
     if (m_mouseButton == Qt::LeftButton) {
@@ -550,6 +551,7 @@ void SceneViewer::onMove(const TMouseEvent &event) {
         getInputManager()->trackEvent(
           0, 0, pos, NULL, NULL,
           false, TToolTimer::ticks() );
+        getInputManager()->processTracks();
       }
     } else if (m_pressure == 0.0) {
       tool->mouseMove(pos, event);
@@ -695,12 +697,14 @@ void SceneViewer::onPress(const TMouseEvent &event) {
     getInputManager()->trackEvent(
       0, 0, pos, &event.m_pressure, NULL,
       false, TToolTimer::ticks() );
+    getInputManager()->processTracks();
   } else if (m_mouseButton == Qt::LeftButton) {
     TApp::instance()->getCurrentTool()->setToolBusy(true);
     tool->leftButtonDown(pos, event);
     getInputManager()->trackEvent(
       0, 0, pos, NULL, NULL,
       false, TToolTimer::ticks() );
+    getInputManager()->processTracks();
   }
   if (m_mouseButton == Qt::RightButton) tool->rightButtonDown(pos, event);
 }
@@ -795,6 +799,7 @@ void SceneViewer::onRelease(const TMouseEvent &event) {
           0, 0, pos,
           (event.m_isTablet ? &event.m_pressure : NULL),
           NULL, true, TToolTimer::ticks() );
+        getInputManager()->processTracks();
       }
       TApp::instance()->getCurrentTool()->setToolBusy(false);
     }
