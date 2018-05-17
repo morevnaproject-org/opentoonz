28feda3a2f3ab4c815144903c086e49b25b83797
Ivan Mahonin <bh@icystar.com>
2018-05-17 15:10:43 +0700

    #assistants: defferent modifiers model for each tool

--- toonz/sources/toonz/sceneviewerevents.cpp
+++ toonz/sources/toonz/sceneviewerevents.cpp
@@ -1581,6 +1581,8 @@ void SceneViewer::onToolSwitched() {
   TTool *tool = TApp::instance()->getCurrentTool()->getTool();
   if (tool) tool->updateMatrix();
 
+  rebuildModifiers();
+
   onLevelChanged();
   update();
 }
