d25475b043938da485d322a0bca2e15d2fa7dbc6
Ivan Mahonin <bh@icystar.com>
2019-08-13 11:24:44 +0700

    #assistants: fix FullColorBrushTool option box

--- toonz/sources/tnztools/fullcolorbrushtool.cpp
+++ toonz/sources/tnztools/fullcolorbrushtool.cpp
@@ -872,6 +872,9 @@ FullColorBrushToolNotifier::FullColorBrushToolNotifier(FullColorBrushTool *tool)
       ret = connect(paletteHandle, SIGNAL(colorStyleSwitched()), this,
                     SLOT(onColorStyleChanged()));
       assert(ret);
+      ret = connect(paletteHandle, SIGNAL(paletteSwitched()), this,
+                    SLOT(onColorStyleChanged()));
+      assert(ret);
     }
   }
 }
