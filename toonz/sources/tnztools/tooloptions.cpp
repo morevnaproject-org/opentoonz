3c6937333c9f3f70b8b5dbdc8b9c77887aa37ee5
Ivan Mahonin <bh@icystar.com>
2020-01-15 02:47:59 +0700

    #assistants: fix bug with extra signals connection

--- toonz/sources/tnztools/tooloptions.cpp
+++ toonz/sources/tnztools/tooloptions.cpp
@@ -2619,8 +2619,16 @@ ToolOptions::~ToolOptions() {}
 
 void ToolOptions::showEvent(QShowEvent *) {
   TTool::Application *app = TTool::getApplication();
-  ToolHandle *currTool    = app->getCurrentTool();
-  if (currTool) {
+
+  if (ToolHandle *currTool = app->getCurrentTool())
+    currTool->disconnect(this);
+  if (TObjectHandle *currObject = app->getCurrentObject())
+    currObject->disconnect(this);
+  if (TXshLevelHandle *currLevel = app->getCurrentLevel())
+    currLevel->disconnect(this);
+  
+  if (ToolHandle *currTool = app->getCurrentTool()) {
+    currTool->disconnect(this);
     onToolSwitched();
     connect(currTool, SIGNAL(toolSwitched()), SLOT(onToolSwitched()));
     connect(currTool, SIGNAL(toolOptionsBoxChanged()),
@@ -2628,19 +2636,18 @@ void ToolOptions::showEvent(QShowEvent *) {
     connect(currTool, SIGNAL(toolChanged()), SLOT(onToolChanged()));
   }
 
-  TObjectHandle *currObject = app->getCurrentObject();
-  if (currObject) {
+  if (TObjectHandle *currObject = app->getCurrentObject()) {
+    currObject->disconnect(this);
     onStageObjectChange();
     connect(currObject, SIGNAL(objectSwitched()), SLOT(onStageObjectChange()));
     connect(currObject, SIGNAL(objectChanged(bool)),
             SLOT(onStageObjectChange()));
   }
 
-  TXshLevelHandle *currLevel = app->getCurrentLevel();
-
-  if (currLevel)
+  if (TXshLevelHandle *currLevel = app->getCurrentLevel()) {
     connect(currLevel, SIGNAL(xshLevelSwitched(TXshLevel *)), this,
             SLOT(onStageObjectChange()));
+  }
 }
 
 //-----------------------------------------------------------------------------
