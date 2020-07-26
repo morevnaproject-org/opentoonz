67773c02f49e3f9123081c884af1a9119440c926
Konstantin Dmitriev <ksee.zelgadis@gmail.com>
2020-07-26 19:51:52 +0700

    Switch scenes with single click.

--- toonz/sources/toonz/tpanels.cpp
+++ toonz/sources/toonz/tpanels.cpp
@@ -1080,7 +1080,7 @@ public:
     TFilePath scenesFolder =
         TProjectManager::instance()->getCurrentProject()->getScenesPath();
     browser->setFolder(scenesFolder, true);
-    browser->enableDoubleClickToOpenScenes();
+    browser->enableSingleClickToOpenScenes();
   }
 } PreproductionBoardFactory;
 
